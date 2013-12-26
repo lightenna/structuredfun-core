8cda87f26c260259f4b76d428cb1c6d1cb6b8430
Alex Stanhope <alex_stanhope@hotmail.com>
2013-12-26 20:34:07 +0000

    added tests for URL substitution

--- src/Lightenna/StructuredBundle/Tests/Controller/ViewControllerTest.php
+++ src/Lightenna/StructuredBundle/Tests/Controller/ViewControllerTest.php
@@ -9,18 +9,21 @@ class ViewControllerTest extends WebTestCase
 {
 	public function testconvertRawToFilename() {
 		$t = new ViewController();
+		// tests being run in this environment without a $_SERVER['DOCUMENT_ROOT']
+		// but can derive from $_SERVER['PHPRC']
+		$_SERVER['DOCUMENT_ROOT'] = str_replace('conf', 'htdocs/web', $_SERVER['PHPRC']);
 		// test path to Symfony
-		$this->assertEquals($t->convertRawToFilename('data/my_directory/'),'/../../../data/my_directory');
+		$this->assertEquals($t->convertRawToFilename('data/my_directory/'),$_SERVER['DOCUMENT_ROOT'].'/../../../data/my_directory');
 		// test path without trailing /
-		$this->assertEquals($t->convertRawToFilename('data/my_directory'),'/../../../data/my_directory');
+		$this->assertEquals($t->convertRawToFilename('data/my_directory'),$_SERVER['DOCUMENT_ROOT'].'/../../../data/my_directory');
 		// test blank path
-		$this->assertEquals($t->convertRawToFilename(''),'/../../../');
+		$this->assertEquals($t->convertRawToFilename(''),$_SERVER['DOCUMENT_ROOT'].'/../../../');
 		// test path /
-		$this->assertEquals($t->convertRawToFilename('/'),'/../../../');
+		$this->assertEquals($t->convertRawToFilename('/'),$_SERVER['DOCUMENT_ROOT'].'/../../../');
 		// test path .zip
-		$this->assertEquals($t->convertRawToFilename('mydir/my.zip'),'/../../../mydir/my.zip');
+		$this->assertEquals($t->convertRawToFilename('mydir/my.zip'),$_SERVER['DOCUMENT_ROOT'].'/../../../mydir/my.zip');
 		// test path .zip to .jpg
-		$this->assertEquals($t->convertRawToFilename('mydir/my.zip'.ZIP_SEPARATOR.'zippath/myzip.jpg'),'/../../../mydir/my.zip'.ZIP_SEPARATOR.'zippath/myzip.jpg');
+		$this->assertEquals($t->convertRawToFilename('mydir/my.zip'.ZIP_SEPARATOR.'zippath/myzip.jpg'),$_SERVER['DOCUMENT_ROOT'].'/../../../mydir/my.zip'.ZIP_SEPARATOR.'zippath/myzip.jpg');
 	}
 
 	public function testConvertRawToUrl() {
@@ -42,28 +45,47 @@ class ViewControllerTest extends WebTestCase
 	public function testGetExtension() {
 		$t = new ViewController();
 		// find an extension
-		$this->assertEquals($t::getExtension('data/my_directory/myfile.ext'),'ext');
+		$this->assertEquals($t::getExtension('data/ext_directory/myfile.ext'),'ext');
 		// find an upper case jpg
-		$this->assertEquals($t::getExtension('data/my_directory/myfile.JPG'),'JPG');
+		$this->assertEquals($t::getExtension('data/ext_directory/myfile.JPG'),'JPG');
 		// find no extension
-		$this->assertEquals($t::getExtension('data/my_directory/myfile'),false);
+		$this->assertEquals($t::getExtension('data/ext_directory/myfile'),false);
 		// find no extension on a directory
-		$this->assertEquals($t::getExtension('data/my_directory/myfile/'),false);
+		$this->assertEquals($t::getExtension('data/ext_directory/myfile/'),false);
 		// find an extension when there are args
-		$this->assertEquals($t::getExtension('data/my_directory/myfile.jpg'.ARG_SEPARATOR.'test'),'jpg');
+		$this->assertEquals($t::getExtension('data/ext_directory/myfile.jpg'.ARG_SEPARATOR.'test'),'jpg');
 		// find an extension when there are args (malformed)
-		$this->assertEquals($t::getExtension('data/my_directory/myfile.jpg'.ARG_SEPARATOR.''.ARG_SEPARATOR.'test'),'jpg');
+		$this->assertEquals($t::getExtension('data/ext_directory/myfile.jpg'.ARG_SEPARATOR.''.ARG_SEPARATOR.'test'),'jpg');
 	}
 
 	public function testGetArgsFromPath() {
-		$t = new \Lightenna\StructuredBundle\Controller\ViewController();
+		$t = new ViewController();
 		// test no args
-		$this->assertEquals($t::getArgsFromPath('data/my_directory/myfile.ext'),array());
+		$this->assertEquals($t::getArgsFromPath('data/arg_directory/myfile.ext'),array());
 		// test 1 arg
-		$this->assertEquals($t::getArgsFromPath('data/my_directory/myfile.ext'.ARG_SEPARATOR.'test=1'),array('test' => 1));
+		$this->assertEquals($t::getArgsFromPath('data/arg_directory/myfile.ext'.ARG_SEPARATOR.'test=1'),array('test' => 1));
 		// test 2 args
-		$this->assertEquals($t::getArgsFromPath('data/my_directory/myfile.ext'.ARG_SEPARATOR.'test=1&k=v'),array('test' => 1, 'k' => 'v'));
+		$this->assertEquals($t::getArgsFromPath('data/arg_directory/myfile.ext'.ARG_SEPARATOR.'test=1&k=v'),array('test' => 1, 'k' => 'v'));
 		// test bad arg
-		$this->assertEquals($t::getArgsFromPath('data/my_directory/myfile.ext'.ARG_SEPARATOR.'test'),array('test' => null));
+		$this->assertEquals($t::getArgsFromPath('data/arg_directory/myfile.ext'.ARG_SEPARATOR.'test'),array('test' => null));
+	}
+
+	public function testPerformFilenameSubstitution() {
+		$t = new ViewController();
+		// test no substitution
+		$this->assertEquals($t->convertRawToFilename('structured/tests/data/'), $t->convertRawToFilename('structured/tests/data/'));
+		// test simple [1]
+		$this->assertEquals($t->convertRawToFilename('structured/tests/[1]/'), $t->convertRawToFilename('structured/tests/data/'));
+		// test two subs
+		$this->assertEquals($t->convertRawToFilename('structured/tests/[1]/[1]'), $t->convertRawToFilename('structured/tests/data/10-file_folder'));
+		// test nth
+		$this->assertEquals($t->convertRawToFilename('structured/tests/[1]/[3]'), $t->convertRawToFilename('structured/tests/data/30-zip_folder.zip'));
+		// test zip extract
+		$this->assertEquals($t->convertRawToFilename('structured/tests/data/30-zip_folder.zip/[1]'), $t->convertRawToFilename('structured/tests/data/30-zip_folder.zip/00980001.JPG'));
+		// test nth and zip extract
+		$this->assertEquals($t->convertRawToFilename('structured/tests/[1]/[3]/[1]'), $t->convertRawToFilename('structured/tests/data/30-zip_folder.zip/00980001.JPG'));
+		// @todo this test fails because we haven't coded for this case yet
+		// test zip extract from nested folder [FAILS]
+		// $this->assertEquals($t->convertRawToFilename('structured/tests/data/30-zip_folder.zip/nested/[1]'), $t->convertRawToFilename('structured/tests/data/30-zip_folder.zip/00980001.JPG'));
 	}
 }
