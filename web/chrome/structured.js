317a8961249cbc4a894c4d558b1ddc1d4c141140
cleverlight <github.account@cleveraspaint.com>
2014-01-05 22:26:45 +0000

    sass/susy grid, image re-srcing on window resize

--- web/chrome/structured.js
+++ web/chrome/structured.js
@@ -6,27 +6,26 @@
 	// constants
 	var numberOfImageVariants = 4;
 
-	// hunt out sfun-thumb images and attach resize listener
-	$(document).ready(function() {
-		$('.sfun-thumb').click(function() {
-			var im = this;
-			// if the image is oversampled
-			if (im.clientWidth > $(im).data('loaded-width')) {
-				// and the original image is bigger than the one we loaded
-				if ($(im).data('loaded-width') < $(im).data('native-width')) {
-					// calculate the variant intervals
-					var interval = $(im).data('native-width') / numberOfImageVariants;
-					// get the variant equal to or slightly bigger than we need
-					var req = (Math.floor(im.clientWidth / interval) + 1) * interval;
-					// swap the image for a bigger one
-					// assumption: no other args on im image
-					var newsrc = $(im).data('base-src') 'maxwidth=' + req;
-					$(im).attr('src', newsrc);
-				}
-			}
-			// don't bubble up the event
-			return(false);
-		});
+	// catch window resize
+	$(window).resize(function () {
+	  // check all the thumbnails
+	   $('.thumb').each(function() {
+	      var im = this;
+	      // if the image is oversampled
+	      if (im.clientWidth > $(im).data('loaded-width')) {
+	        // and the original image is bigger than the one we loaded
+	        if ($(im).data('loaded-width') < $(im).data('native-width')) {
+	          // calculate the variant intervals
+	          var interval = $(im).data('native-width') / numberOfImageVariants;
+	          // get the variant equal to or slightly bigger than we need
+	          var req = (Math.floor(im.clientWidth / interval) + 1) * interval;
+	          // swap the image for a bigger one
+	          // assumption: no other args on im image
+	          var newsrc = $(im).data('base-src') + 'maxwidth=' + req;
+	          $(im).attr('src', newsrc);
+	        }
+	      }
+	    });
 	});
-
+	
 })(jQuery, undefined);
