7e557ecbc9f6fffb5a5befdcdb3c8c7d36feeb4b
cleverlight <github.account@cleveraspaint.com>
2014-01-31 21:46:43 +0000

    test suite running under linux

---
+++ src/Lightenna/StructuredBundle/DependencyInjection/MetadataFileReader.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Lightenna\StructuredBundle\DependencyInjection;
+class MetadataFileReader extends FileReader {
+
+  /**
+   * Overriden getListing function that adds metadata to the elements
+   * @see \Lightenna\StructuredBundle\DependencyInjection\FileReader::getListing()
+   */
+
+  public function getListing() {
+    $listing = parent::getListing();
+    // loop through each obj (object refs have implicit &$obj)
+    foreach ($listing as $obj) {
+      $obj->{'ext'} = self::getExtension($obj->{'name'});
+      // catch hidden files
+      if ($obj->{'name'}[0] == '.') {
+        $obj->{'hidden'} = true;
+      }
+      // type image based on extension
+      switch (strtolower($obj->{'ext'})) {
+        case 'jpeg':
+        case 'jpg':
+        case 'gif':
+          $obj->{'type'} = 'image';
+          break;
+        case 'mp4':
+        case 'm4v':
+        case 'avi':
+        case 'flv':
+          $obj->{'type'} = 'video';
+          break;
+        case 'zip':
+          $obj->{'type'} = 'directory';
+          break;
+      }
+      // get the image metadata to calculate display properties
+      // $obj->orientation = self::getImageOrientation($obj);
+    }
+    return $listing;
+  }
+}
--- src/Lightenna/StructuredBundle/Tests/Controller/ImageviewControllerTest.php
+++ src/Lightenna/StructuredBundle/Tests/Controller/ImageviewControllerTest.php
@@ -18,38 +18,45 @@ class ImageviewControllerTest extends WebTestCase
 		// test width-limited landscape resize
 		$t->setArgs(array('maxwidth' => $restricted));
 		$t->imageCalcNewSize($wide);
-		$this->assertEquals($t->getStats()['newwidth'], $restricted);
-		$this->assertEquals($t->getStats()['newheight'] < $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'], $restricted);
+		$this->assertEquals($stats['newheight'] < $restricted, true);
 		// test height-limited portrait resize
 		$t->setArgs(array('maxheight' => $restricted));
 		$t->imageCalcNewSize($tall);
-		$this->assertEquals($t->getStats()['newwidth'] < $restricted, true);
-		$this->assertEquals($t->getStats()['newheight'], $restricted);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'] < $restricted, true);
+		$this->assertEquals($stats['newheight'], $restricted);
 		// test unrestricted resize
 		$t->setArgs(array());
 		$t->imageCalcNewSize($wide);
-		$this->assertEquals($t->getStats()['newwidth'], $long);
-		$this->assertEquals($t->getStats()['newheight'], $short);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'], $long);
+		$this->assertEquals($stats['newheight'], $short);
 		// test weird width-limited portrait resize
 		$t->setArgs(array('maxwidth' => $restricted));
 		$t->imageCalcNewSize($tall);
-		$this->assertEquals($t->getStats()['newwidth'], $restricted);
-		$this->assertEquals($t->getStats()['newheight'] > $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'], $restricted);
+		$this->assertEquals($stats['newheight'] > $restricted, true);
 		// test width-limited landscape resize using longest
 		$t->setArgs(array('maxlongest' => $restricted));
 		$t->imageCalcNewSize($wide);
-		$this->assertEquals($t->getStats()['newwidth'], $restricted);
-		$this->assertEquals($t->getStats()['newheight'] < $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'], $restricted);
+		$this->assertEquals($stats['newheight'] < $restricted, true);
 		// test height-bound landscape resize using shortest
 		$t->setArgs(array('maxshortest' => $restricted));
 		$t->imageCalcNewSize($wide);
-		$this->assertEquals($t->getStats()['newwidth'] > $restricted, true);
-		$this->assertEquals($t->getStats()['newheight'] == $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'] > $restricted, true);
+		$this->assertEquals($stats['newheight'] == $restricted, true);
 		// test height-limited portrait resize using longest
 		$t->setArgs(array('maxlongest' => $restricted));
 		$t->imageCalcNewSize($tall);
-		$this->assertEquals($t->getStats()['newwidth'] < $restricted, true);
-		$this->assertEquals($t->getStats()['newheight'] == $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'] < $restricted, true);
+		$this->assertEquals($stats['newheight'] == $restricted, true);
 	}
 
 	public function testfilterImage() {
@@ -65,19 +72,22 @@ class ImageviewControllerTest extends WebTestCase
 		$t->setArgs(array('clipwidth' => $restricted, 'clipheight' => $restricted));
 		$wide_data_copy = $t::getImageData($wide);
 		$t->filterImage($wide_data_copy);
-		$this->assertEquals($t->getStats()['newwidth'] == $restricted, true);
-		$this->assertEquals($t->getStats()['newheight'] == $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'] == $restricted, true);
+		$this->assertEquals($stats['newheight'] == $restricted, true);
 		// test expanding using clipping functions [not really supported]
 		$t->setArgs(array('clipwidth' => $restricted, 'clipheight' => 2*$long));
 		$data_copy = $t::getImageData($wide);
 		$t->filterImage($data_copy);
-		$this->assertEquals($t->getStats()['newwidth'] == $restricted, true);
-		$this->assertEquals($t->getStats()['newheight'] == 2*$long, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'] == $restricted, true);
+		$this->assertEquals($stats['newheight'] == 2*$long, true);
 		// test height-bound landscape resize using shortest, followed by a clip
 		$t->setArgs(array('maxshortest' => $restricted, 'clipwidth' => $restricted, 'clipheight' => $restricted));
 		$data_copy = $t::getImageData($tall);
 		$t->filterImage($data_copy);
-		$this->assertEquals($t->getStats()['newwidth'] == $restricted, true);
-		$this->assertEquals($t->getStats()['newheight'] == $restricted, true);
+		$stats=$t->getStats();
+		$this->assertEquals($stats['newwidth'] == $restricted, true);
+		$this->assertEquals($stats['newheight'] == $restricted, true);
 	}
 }
