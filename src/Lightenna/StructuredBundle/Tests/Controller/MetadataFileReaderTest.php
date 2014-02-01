4db8e55ec4da9c09ce0273896c4003f0143a153c
cleverlight <github.account@cleveraspaint.com>
2014-02-01 09:00:24 +0000

    advanced FileReader

--- src/Lightenna/StructuredBundle/Tests/Controller/MetadataFileReaderTest.php
+++ src/Lightenna/StructuredBundle/Tests/Controller/MetadataFileReaderTest.php
@@ -1,17 +1,25 @@
 <?php
 
 namespace Lightenna\StructuredBundle\Tests\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Lightenna\StructuredBundle\Controller\ViewController;
 use Lightenna\StructuredBundle\DependencyInjection\MetadataFileReader;
 
-class MetadataFileReaderTest extends WebTestCase
-{
-	public function testGetListing() {
-	  $t = new ViewController();
-		$mfr = new MetadataFileReader($t->convertRawToFilename('/structured/tests/data'));
-		$first = reset($mfr->getListing());
-		$this->assertEquals($first->{'name'},'10-file_folder');
-	}
+class MetadataFileReaderTest extends WebTestCase {
+
+  public function testGetListing() {
+    $t = new ViewController();
+    // read first entry in standard folder
+    $mfr = new MetadataFileReader($t->convertRawToFilename('/structured/tests/data'));
+    $first = reset($mfr->getListing());
+    $this->assertEquals($first->{'name'}, '10-file_folder');
+    // read first entry in zip folder
+    $mfr = new MetadataFileReader($t->convertRawToFilename('/structured/tests/data/30-zip_folder.zip'));
+    $first = reset($mfr->getListing());
+    $this->assertEquals($first->{'name'}, '00980001.JPG');
+    // read first entry in folder inside zip folder
+    $mfr = new MetadataFileReader($t->convertRawToFilename('/structured/tests/data/30-zip_folder.zip/nested'));
+    $first = reset($mfr->getListing());
+    $this->assertEquals($first->{'name'}, '00980006.JPG');
+  }
 }
