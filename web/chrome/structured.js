3f0ef0b96d2e124c1137c63456ced4517bef93eb
cleverlight <github.account@cleveraspaint.com>
2014-03-22 22:04:35 +0000

    progress towards pc view

--- web/chrome/structured.js
+++ web/chrome/structured.js
@@ -4,26 +4,17 @@
 (function($, undefined) {
 
   var resizeTimeout = null;
-  var debug = false;
+  var debug = true;
 
   this.init = function() {
     var that = this;
     $(document).ready(function(){
       // find all imagebind containers, setup images to listen for load and bind
       $('.cell').each(function() {
-        // read container width/height
-        var cx = $(this).width(), cy = $(this).height();
-        var cratio = cx / cy;
+        var jqCell = $(this);
         // bind to image's loaded event
-        $(this).find('img').load(function() {
-          // detect if the image is bound by width/height in this container
-          var ix = $(this).width(), iy = $(this).height();
-          var iratio = ix / iy;
-          // console.log('cx['+cx+'] cy['+cy+'] cratio['+cratio+'], ix['+ix+'] iy['+iy+'] iratio['+iratio+']: '+(cratio / iratio).toPrecision(3)+'= '+direction+'-bound');
-          var direction = ((cratio / iratio) > 1.0 ? 'y' : 'x');
-          var invdir = (direction == 'x' ? 'y' : 'x');
-          // apply class to image
-          $(this).addClass(direction+'-bound').removeClass(invdir+'-bound');
+        $(this).find('img.bounded').load(function() {
+          that.checkImageBound(jqCell, $(this));
         });
       });
       // find all screenpc elements, extract pc and store as data- attribute
@@ -45,7 +36,7 @@
         }
         that.resizeTimeout = setTimeout(function() {
           that.refresh();
-        }, 30);
+        }, 50); // 50 ms
       });
     });
   };
@@ -54,13 +45,26 @@
    * Refresh element width/heights when screen size changes
    */
   this['refresh'] = function() {
+    var that = this;
     var win = $(window);
-    var ww = $(window).width(), wh = win.height();
+    var ww = win.width(), wh = win.height();
     var pcwidth = undefined, pcheight = undefined;
+    // try to find a yardstick-(x/y) and use if found for width/height
+    var yardx = $('#yardstick-x'), yardy = $('#yardstick-y');
+    if (yardx.length) {
+      ww = yardx.width();
+    }
+    if (yardy.length) {
+      wh = yardy.height();
+    }
+    if (debug) {
+      console.log('viewport w['+ww+'] h['+wh+']');
+    }
+    // loop through all 'ready' elements
     $('.screenpc-ready').each(function() {
       // read data-screen-pc-width|height if set
-      pcwidth = $(this).data("screenpc-width");
-      pcheight = $(this).data("screenpc-height");
+      pcwidth = $(this).data('screenpc-width');
+      pcheight = $(this).data('screenpc-height');
       // apply screen percentage as px
       if (pcwidth) {
         $(this).width(ww * pcwidth / 100);
@@ -68,8 +72,14 @@
       if (pcheight) {
         $(this).height(wh * pcheight / 100);
       }
-      if (debug) {
-        console.log("screenpc["+$(this).attr('id')+"] w["+pcwidth+"%] h["+pcheight+"%] now w["+$(this).width()+"] h["+$(this).height()+"]");
+      if (debug && false) {
+        console.log('screenpc['+$(this).attr('id')+'] w['+pcwidth+'%] h['+pcheight+'%] now w['+$(this).width()+'] h['+$(this).height()+']');
+      }
+      // after resizing elements:
+      // 1. look to see if the x-bound/y-bound has changed
+      // 2. change out the image for a better resolution
+      if (pcwidth || pcheight) {
+        that.checkImageBound($(this), $(this).find('img.bounded'));
       }
     });
   };
@@ -81,7 +91,7 @@
    */
   this['generate_data_pc'] = function(jq, axis) {
     var elemclass, elemid, elempc = undefined;
-    // parse stylesheets for id:width
+    // parse stylesheets for #id:width
     elemid = jq.attr('id');
     if (elemid != undefined) {
       // parse stylesheets for class(n):width
@@ -110,7 +120,7 @@
   
   /**
    * Search for a property in a stylesheet class
-   * @todo optimise
+   * @todo optimise, suggest caching of found rules
    * @param string element selector
    * @param string property to search for
    * @param [string] matchstrip characters to match and strip from the result
@@ -128,7 +138,10 @@
       for (var i=0 ; i < rules.length ; i++) {
         var rule = rules[i];
         // test rule name against elem
-        if (rule.selectorText == elem) {
+        if (endswith(rule.selectorText, elem)) {
+          if (debug && false) {
+            console.log('matched rule['+rule.selectorText+'] against elem['+elem+']');
+          }
           var elempc = rule.style.getPropertyValue(prop);
           // if we actually found that property in this stylesheet class
           if (elempc != undefined) {
@@ -151,6 +164,37 @@
     return undefined;
   };
 
+  /**
+   * @param string to search within
+   * @param string to search for
+   * @return true if the haystack ends with the needle
+   */
+  this.endswith = function(haystack, needle) {
+    // roll on ECMAScript 6
+    return haystack.indexOf(needle, haystack.length - needle.length) !== -1;
+  };
+  
+  /**
+   * update (x/y)-bound on image
+   * @param jQuery object parent container, which has changed size
+   * @param jQuery object bounded image within
+   */
+  this['checkImageBound'] = function(jqCell, jqImg) {
+    // read container width/height
+    var cx = jqCell.width(), cy = jqCell.height();
+    var cratio = cx / cy;
+    // detect if the image is bound by width/height in this container
+    var ix = jqImg.width(), iy = jqImg.height();
+    var iratio = ix / iy;
+    var direction = ((cratio / iratio) > 1.0 ? 'y' : 'x');
+    var invdir = (direction == 'x' ? 'y' : 'x');
+    if (debug && false) {
+      console.log('cx['+cx+'] cy['+cy+'] cratio['+cratio+'], ix['+ix+'] iy['+iy+'] iratio['+iratio+']: '+(cratio / iratio).toPrecision(3)+'= '+direction+'-bound');
+    }
+    // apply class to image
+    jqImg.addClass(direction+'-bound').removeClass(invdir+'-bound');
+  };
+  
   // call init function
   this.init();
 
