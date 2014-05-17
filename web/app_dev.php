98ec9038387ee62065512e82639b6c9909d27f5f
cleverlight <github.account@cleveraspaint.com>
2014-05-17 20:01:31 +0100

    reduced security on app_dev

--- web/app_dev.php
+++ web/app_dev.php
@@ -12,7 +12,8 @@ use Symfony\Component\Debug\Debug;
 if ((isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
-) && (!in_array(@$_SERVER['HTTP_HOST'], array('sfun.elan', '10.12.1.160')))) {
+// disable this security test (using && false) to allow cross-network debugging
+) && (false)) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
