a2fb843630a89cbb7d75c521c986b4dec292b8d5
Alex Stanhope <alex_stanhope@hotmail.com>
2014-06-19 22:25:22 +0100

    updated isVis, work to do on testing

--- web/chrome/structured-QUnit.js
+++ web/chrome/structured-QUnit.js
@@ -64,7 +64,7 @@
      */
     test( 'check image bounds', function() {
       QUnit.stop();
-      $('ul.flow li.cell img.bounded').each(function() {
+      $('ul.flow .selectablecell .boundable').each(function() {
         var imw = $(this).width(), imh = $(this).height();
         var cellw = $(this).parents('li').width(), cellh = $(this).parents('li').height();
         var withinWidth = (imw <= cellw);
@@ -81,48 +81,49 @@
 
     test( 'image click #0', function() {
       window.location.hash = 'image_click_0';
-      $('#imgseq-0').parents('a').trigger('click');
+      $('#seq-0').find('a').trigger('click');
       ok( sfun.api_getBreadth() == 1, 'Went to fullscreen mode' );
-      ok( $('ul.flow li.cell img.selected').data('seq') == 0, 'Selected image zero');
+      ok( $('ul.flow .selectablecell.selected').data('seq') == 0, 'Selected image zero');
       window.location.hash = '';
     });
 
     test( 'image click #1, return, arrow next', function() {
       window.location.hash = 'image_click_1';
       var initialBreadth = sfun.api_getBreadth();
-      $('#imgseq-1').parents('a').trigger('click');
+      $('#seq-1').find('a').trigger('click');
       sfun.api_triggerKeypress(sfun.KEY_RETURN);
       ok( sfun.api_getBreadth() == initialBreadth, 'Returned to initial breadth value ('+initialBreadth+')' );
       sfun.api_triggerKeypress(sfun.KEY_ARROW_RIGHT);
-      ok( $('ul.flow li.cell img.selected').data('seq') == 2, 'Right arrow selected #2 image (#1+1)' );
+      ok( $('ul.flow .selectablecell.selected').data('seq') == 2, 'Right arrow selected #2 image (#1+1)' );
       window.location.hash = '';
     });
 
     test( 'end select last, home select first, arrow next', function() {
       window.location.hash = 'end_select_last';
-      $('#imgseq-0').parents('a').trigger('click');
+      $('#seq-0').parents('a').trigger('click');
       sfun.api_triggerKeypress(sfun.KEY_END);
-      ok( $('ul.flow li.cell img.selected').data('seq') == (sfun.api_getTotalEntries()-1), 'End selected last image' );
+      ok( $('ul.flow .selectablecell.selected').data('seq') == (sfun.api_getTotalEntries()-1), 'End selected last image' );
       sfun.api_triggerKeypress(sfun.KEY_HOME);
-      ok( $('ul.flow li.cell img.selected').data('seq') == 0, 'Home selected #0 image' );
+      ok( $('ul.flow .selectablecell.selected').data('seq') == 0, 'Home selected #0 image' );
       sfun.api_triggerKeypress(sfun.KEY_ARROW_RIGHT);
-      ok( $('ul.flow li.cell img.selected').data('seq') == 1, 'Right arrow selected #1 image' );
+      ok( $('ul.flow .selectablecell.selected').data('seq') == 1, 'Right arrow selected #1 image' );
       window.location.hash = '';
     });
 
-    test( 'end arrow next wrap-around', function() {
-      var last = sfun.api_getTotalEntries()-1;
-      window.location.hash = 'end_arrow_next';
-      sfun.api_triggerKeypress(sfun.KEY_END);
-      ok( $('ul.flow li.cell img.selected').data('seq') == last, 'End selected last image' );
-      sfun.api_triggerKeypress(sfun.KEY_ARROW_RIGHT);
-      ok( $('ul.flow li.cell img.selected').data('seq') == 0, 'Right arrow selected #0 image' );
-      sfun.api_triggerKeypress(sfun.KEY_END);
-      ok( $('ul.flow li.cell img.selected').data('seq') == last, 'End re-selected last image' );
-      sfun.api_triggerKeypress(sfun.KEY_HOME);
-      ok( $('ul.flow li.cell img.selected').data('seq') == 0, 'Home selected #0 image' );
-      window.location.hash = '';
-    });
+    // test( 'end arrow next wrap-around', function() {
+    //   var last = sfun.api_getTotalEntries()-1;
+    //   window.location.hash = 'end_arrow_next';
+    //   sfun.api_triggerKeypress(sfun.KEY_END);
+    //   ok( $('ul.flow .selectablecell.selected').data('seq') == last, 'End selected last image' );
+    //   sfun.api_triggerKeypress(sfun.KEY_ARROW_RIGHT);
+    //   ok( $('ul.flow .selectablecell.selected').data('seq') == 0, 'Right arrow selected #0 image' );
+    //   sfun.api_triggerKeypress(sfun.KEY_END);
+    //   ok( $('ul.flow .selectablecell.selected').data('seq') == last, 'End re-selected last image' );
+    //   sfun.api_triggerKeypress(sfun.KEY_HOME);
+    //   ok( $('ul.flow .selectablecell.selected').data('seq') == 0, 'Home selected #0 image' );
+    //   window.location.hash = '';
+    // });
+
     QUnit.start();
   }
 
