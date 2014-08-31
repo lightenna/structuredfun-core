d1822d4258eab15c98cc16f806b90a448493c152
Alex Stanhope <alex_stanhope@hotmail.com>
2014-08-31 21:02:46 +0100

    clean run on test suite, but intermittent failure in Firefox needs more investigation

--- web/chrome/structured-QUnit-single.js
+++ web/chrome/structured-QUnit-single.js
@@ -61,6 +61,54 @@
       window.location.hash = '#!';
     }
 
+    test( 'rapid scroll event queuing', function() {
+      window.location.hash = 'rapid-scroll';
+      var direction = sfun.api_getDirection();
+      QUnit.stop();
+      sfun.api_triggerKeypress(sfun.KEY_HOME).done(function() {
+        var initialSeq = $('ul.flow .selectablecell.selected').data('seq');
+        // scroll to last element
+        sfun.api_triggerKeypress(sfun.KEY_END).done(function() {
+          var finalSeq = sfun.api_getTotalEntries()-1;
+          var $selected = $('ul.flow .selectablecell.selected');
+          equal( $selected.data('seq'), finalSeq, 'Selected last image (#'+finalSeq+')' );
+          // store major axis position of final image then use to calculate scroll limit
+          var position = $selected.offset();
+          var limitPos = (direction == 'x' ? position.left - sfun.api_getViewportWidth() : position.top - sfun.api_getViewportHeight());
+          // scroll to middle
+          var middleSeq = Math.floor((finalSeq - initialSeq) / 2);
+          // scroll to middle image
+          sfun.api_imageAdvanceTo(middleSeq).done(function() {
+            $selected = $('ul.flow .selectablecell.selected');
+            equal($selected.data('seq'), middleSeq, 'Selected middle image (#'+middleSeq+')' );
+            // use position of middle image as startPos
+            var position = $selected.offset();
+            var startPos = (direction == 'x' ? position.left : position.top);
+            // check that we have 1000 pixels to scroll within
+            if (limitPos - startPos < 1000) {
+              ok(true, 'image set not large enough to conduct scroll test');
+            } else {
+              var target = { 'left': 0, 'top': 0 };
+              // fire 100 x 10px scroll events in sequence
+              for (var i=0 ; i<100 ; i++) {
+                if (direction == 'x') {
+                  target.left = startPos + (i * 10);
+                } else {
+                  target.top = startPos + (i * 10);
+                }
+                // fire, but not using api_triggerScroll, as browser drag doesn't do that!
+                $(document).scrollLeft(target.left);
+                $(document).scrollTop(target.top);
+              }
+// START HERE
+            }
+            QUnit.start();
+            endTest();
+          });
+        });
+      });
+    });
+
     QUnit.start();
   }
 
