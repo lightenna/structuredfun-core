a2b91ec6279249adaf6a0bdc107d3603c13ea5b6
Alex Stanhope <alex_stanhope@hotmail.com>
2015-05-31 19:25:10 +0100

    found nasty PHP Windows bug that means .zip files get wiped

--- src/Lightenna/StructuredBundle/DependencyInjection/FileReader.php
+++ src/Lightenna/StructuredBundle/DependencyInjection/FileReader.php
@@ -138,11 +138,16 @@ class FileReader {
     if ($this->inZip()) {
       $zip = zip_open($this->file_part);
       $listing = array();
-      if ($zip) {
+      if (is_resource($zip)) {
         while ($zip_entry = zip_read($zip)) {
           $listing[] = zip_entry_name($zip_entry);
         }
         zip_close($zip);
+      } else {
+        // if we didn't get a listing back
+        print(self::errorMessageZip($zip));
+        var_dump($this->file_part);
+        exit;
       }
       // if zip_part_leaf is set and has characters
       if (($this->zip_part_leaf !== null) && ($len = strlen($this->zip_part_leaf)) > 0) {
@@ -387,13 +392,52 @@ class FileReader {
   /**
    * Detect if a file component is an archive 
    */
-
   static function detectZip($comp) {
     $zip_pos = strpos($comp, '.' . Constantly::ZIP_EXTMATCH);
     if ($zip_pos !== false) {
       $zip_pos += strlen(Constantly::ZIP_EXTMATCH) + 1;
     }
     return $zip_pos;
+  }
+
+  /**
+   * @return [string] return zip error number as a meaningful error message
+   */
+  static function errorMessageZip($errno) {
+    // using constant name as a string to make this function PHP4 compatible
+    // http://php.net/manual/en/function.zip-open.php
+    $zipFileFunctionsErrors = array(
+      'ZIPARCHIVE::ER_MULTIDISK' => 'Multi-disk zip archives not supported.',
+      'ZIPARCHIVE::ER_RENAME' => 'Renaming temporary file failed.',
+      'ZIPARCHIVE::ER_CLOSE' => 'Closing zip archive failed',
+      'ZIPARCHIVE::ER_SEEK' => 'Seek error',
+      'ZIPARCHIVE::ER_READ' => 'Read error',
+      'ZIPARCHIVE::ER_WRITE' => 'Write error',
+      'ZIPARCHIVE::ER_CRC' => 'CRC error',
+      'ZIPARCHIVE::ER_ZIPCLOSED' => 'Containing zip archive was closed',
+      'ZIPARCHIVE::ER_NOENT' => 'No such file.',
+      'ZIPARCHIVE::ER_EXISTS' => 'File already exists',
+      'ZIPARCHIVE::ER_OPEN' => 'Can\'t open file',
+      'ZIPARCHIVE::ER_TMPOPEN' => 'Failure to create temporary file.',
+      'ZIPARCHIVE::ER_ZLIB' => 'Zlib error',
+      'ZIPARCHIVE::ER_MEMORY' => 'Memory allocation failure',
+      'ZIPARCHIVE::ER_CHANGED' => 'Entry has been changed',
+      'ZIPARCHIVE::ER_COMPNOTSUPP' => 'Compression method not supported.',
+      'ZIPARCHIVE::ER_EOF' => 'Premature EOF',
+      'ZIPARCHIVE::ER_INVAL' => 'Invalid argument',
+      'ZIPARCHIVE::ER_NOZIP' => 'Not a zip archive',
+      'ZIPARCHIVE::ER_INTERNAL' => 'Internal error',
+      'ZIPARCHIVE::ER_INCONS' => 'Zip archive inconsistent',
+      'ZIPARCHIVE::ER_REMOVE' => 'Can\'t remove file',
+      'ZIPARCHIVE::ER_DELETED' => 'Entry has been deleted',
+    );
+    $errmsg = 'unknown';
+    foreach ($zipFileFunctionsErrors as $constName => $errorMessage) {
+      if (defined($constName) and constant($constName) === $errno) {
+        return 'Zip File Function error: '.$errorMessage;
+      }
+    }
+    return 'Zip File Function error: unknown';
   }
 
   /**
