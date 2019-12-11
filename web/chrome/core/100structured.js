ec70d9788f983b286e0705a0cacc4cd19125b66e
cleverlight <cleverlight@users.noreply.github.com>
2019-12-11 10:10:33 +0000

    minor improvement to merge function before transferring to ZB

--- web/chrome/core/100structured.js
+++ web/chrome/core/100structured.js
@@ -2774,7 +2774,9 @@ window.sfun = (function ($, undefined) {
          */
         'merge': function (obj1, obj2) {
             for (var attrname in obj2) {
-                obj1[attrname] = obj2[attrname];
+                if (obj2.hasOwnProperty(attrname)) {
+                    obj1[attrname] = obj2[attrname];
+                }
             }
             return obj1;
         },
